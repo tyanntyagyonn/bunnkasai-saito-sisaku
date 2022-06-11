4318da9700e800485946c3a255f60b552a901d61
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 16:52:56 +0900

    あ

--- web.py
+++ web.py
@@ -1,6 +1,25 @@
 import streamlit as st
 st.title("科学部")
 st.sidebar.title("目次")
+st.sidebar.button("科学部　文化祭")
 st.sidebar.button("科学部　工学班")
 st.sidebar.button("科学部　化学班")
 st.sidebar.button("科学部　生物班")
+
+def 工学班():
+    st.title("工学班")
+
+def 化学班():
+    st.title("化学班")
+
+def 生物班():
+    st.title("生物班")
+
+if st.sidebar.button("科学部　工学班"):
+    工学班
+
+if st.sidebar.button("科学部　化学班"):
+    化学班
+
+if st.sidebar.button("科学部　生物班"):
+    生物班
