a83ff1e0fc29eca016b25c03b260832df09c9556
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 13:06:07 +0900

    main

--- web.py
+++ web.py
@@ -1,11 +1,6 @@
 import streamlit as st
 st.title("科学部")
-st.sidebar.selectbox(
+st.sidebar(
     "How would you like to be contacted?",
     ("Email", "Home phone", "MObile phone")
 )
-if st.button("Button1"):
-   st.write("Button1 clicked")
-
-if st.button("Button2"):
-   st.write("Button2 clicked")
