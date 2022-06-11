47d1afa697546e36f8b79c18f855e17ea297767d
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 17:01:09 +0900

    あ

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("科学部")
 st.sidebar.title("目次")
 st.sidebar.button("科学部　文化祭")
-st.sidebar.button("科学部工学班")
+st.button("科学部工学班")
 st.sidebar.button("科学部　化学班")
 st.sidebar.button("科学部　生物班")
 
