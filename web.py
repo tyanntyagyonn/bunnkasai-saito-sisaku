a378a9ba8c85411379dc75e2c0a72f91ecdaaafc
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 13:00:21 +0900

    あ

--- web.py
+++ web.py
@@ -1,25 +1,4 @@
 import streamlit as st
-# タイトル
-st.title('科学部')
-# ヘッダ
-st.header('科学部')
-# 純粋なテキスト
-st.text('科学部')
-# サブレベルヘッダ
-st.subheader('科学部')
-# マークダウンテキスト
-st.markdown('科学部')
-# LaTeX テキスト
-st.latex(r'\bar{X} = \frac{1}{N} \sum_{n=1}^{N} x_i')
-# コードスニペット
-st.code('科学部')
-# エラーメッセージ
-st.error('科学部')
-# 警告メッセージ
-st.warning('科学部')
-# 情報メッセージ
-st.info('科学部')
-# 成功メッセージ
-st.success('科学部')
-# 例外の出力
-st.exception(Exception('科学部'))
+import numpy as np
+sample1 = np.array([0,1,2])
+print(sample1)
