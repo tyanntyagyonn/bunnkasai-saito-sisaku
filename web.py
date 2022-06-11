bdfa4042b6c83dd7e0894cc6011bf66950658a99
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 13:13:26 +0900

    あ

--- web.py
+++ web.py
@@ -1,5 +1,10 @@
 import streamlit as st
 st.title("科学部")
 st.sidebar.button(
-    ("工学班","化学班","生物班")
+    ("工学班")
 )
+if st.button("Button1"):
+   st.write("Button1 clicked")
+
+if st.button("Button2"):
+   st.write("Button2 clicked")
