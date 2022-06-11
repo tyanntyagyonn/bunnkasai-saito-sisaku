e8c37ee2cbcb7b8dd585a36d3a659d933cf97e56
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 16:23:20 +0900

    あ

--- web.py
+++ web.py
@@ -1,13 +1,5 @@
 import streamlit as st
-#ワイド表示
-st.set_page_config(layout="wide")
-#セレクトボックスのリストを作成
-pagelist = ["page1","page2"]
-#サイドバーのセレクトボックスを配置
-selector=st.sidebar.selectbox( "ページ選択",pagelist)
-if selector=="page1":
-    if st.button('ページ1ボタン'):
-        st.title("ページ1のタイトル")
-elif selector=="page2":
-    if st.button('ページ2ボタン'):
-        st.title("ページ2のタイトル")
+st.title("科学部")
+st.sidebar.button("科学部　工学班")
+st.sidebar.button("科学部　化学班")
+st.sidebar.button("科学部　生物班")
