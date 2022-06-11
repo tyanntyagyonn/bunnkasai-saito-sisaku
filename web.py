14b7722e714b607f39dd79cddbcf82ca41b6bff7
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 17:02:47 +0900

    あ

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("科学部")
 st.sidebar.title("目次")
 st.sidebar.button("科学部　文化祭")
-st.button("科学部工学班")
+st.sidebar.button("科学部工学班")
 st.sidebar.button("科学部　化学班")
 st.sidebar.button("科学部　生物班")
 
@@ -12,7 +12,7 @@ def page1():
 def page2():
    st.write("Here is page 2.")
 
-if st.sidebar.button("Button1"):
+if st.sidebar.button("科学部工学班"):
    page1()
 
 if st.sidebar.button("Button2"):
