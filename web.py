ff06d0165b1a98d58ef50f1cedacdb37766f1353
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 17:16:20 +0900

    a

--- web.py
+++ web.py
@@ -1,17 +1,31 @@
 import streamlit as st
+ 
+最初 = True
 
-st.title("科学部")
+#複数ページの設置
+
+def 初期ページ():
+    st.title("科学部　文化祭　特設サイト")
+    最初 = False
+
+def 工学班():
+   st.title("工学班")
+   最初 = False
+
+def 生物班():
+   st.title("生物班")
+   最初 = False
+
+def 化学班():
+   st.title("化学班")
+   最初 = False
+
+#サイドバーの設定およびページ移動
 
 st.sidebar.title("目次")
 
-def 工学班():
-   st.write("工学班")
-
-def 生物班():
-   st.write("生物班")
-
-def 化学班():
-   st.write("化学班")
+if st.sidebar.button("科学部　文化祭"):
+    最初 = True
 
 if st.sidebar.button("科学部　工学班"):
    工学班()
@@ -21,3 +35,6 @@ if st.sidebar.button("科学部　生物班"):
 
 if st.sidebar.button("科学部　化学班"):
    化学班()
+
+if 最初 == True:
+    初期ページ()
