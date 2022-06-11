38f7e7411d2fc53a01b6fee31127f026a60bb9d3
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 12:50:46 +0900

    あ

--- web.py
+++ web.py
@@ -1,3 +1,25 @@
 import streamlit as st
-st.title("科学部")
-st.text("工学班" / "生物班")
+# タイトル
+st.title('科学部')
+# ヘッダ
+st.header('科学部')
+# 純粋なテキスト
+st.text('科学部')
+# サブレベルヘッダ
+st.subheader('科学部')
+# マークダウンテキスト
+st.markdown('科学部')
+# LaTeX テキスト
+st.latex(r'\bar{X} = \frac{1}{N} \sum_{n=1}^{N} x_i')
+# コードスニペット
+st.code('科学部')
+# エラーメッセージ
+st.error('科学部')
+# 警告メッセージ
+st.warning('科学部')
+# 情報メッセージ
+st.info('科学部')
+# 成功メッセージ
+st.success('科学部')
+# 例外の出力
+st.exception(Exception('科学部'))
