afacc8fb806eab2b6e77f556bbf099290f3a16cf
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-11 13:04:24 +0900

    main

--- web.py
+++ web.py
@@ -4,3 +4,8 @@ st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "MObile phone")
 )
+if st.button("Button1"):
+   st.write("Button1 clicked")
+
+if st.button("Button2"):
+   st.write("Button2 clicked")
