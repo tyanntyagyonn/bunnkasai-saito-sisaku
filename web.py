ffbc5cba6d0ad7747254a3ea503bd7f0a036183c
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-12 15:31:53 +0900

    あ

--- web.py
+++ web.py
@@ -7,8 +7,6 @@ if 'pezibangou' not in st.session_state:
 
    st.session_state.pezibangou = 0
 
-def 初期ページ():
-   st.title("中等部科学部")
 def 工学班():
    st.title("工学班")
 def 生物班():
@@ -18,9 +16,6 @@ def 化学班():
 
 def ボタン():
    
-   if st.button("中等部科学部掲示板 "):
-      st.session_state.pezibangou = 0
-      初期ページ()
    if st.button("科学部工学班掲示板 "):
       st.session_state.pezibangou = 1
       工学班()
@@ -51,14 +46,14 @@ def ボタン():
 
 def ページ移動():
 
-   if st.session_state.pezibangou == 0:
-      初期ページ()
-   elif st.session_state.pezibangou == 1:
+   if st.session_state.pezibangou == 1:
       工学班()
    elif st.session_state.pezibangou == 2:
       生物班()
    else:
       化学班()
+
+st.title("中等部科学部掲示板")
 
 ページ移動()
 
