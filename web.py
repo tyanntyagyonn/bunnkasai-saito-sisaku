384a84f593d47bede9dadbba2ad17d796d5ebd25
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-12 14:34:21 +0900

    s

--- web.py
+++ web.py
@@ -1,9 +1,28 @@
 import streamlit as st
 
 
+
 #初期設定
+
+def 初期ページ():
+   st.title("中等部科学部")
+
+def 工学班():
+   st.title("工学班")
+
+def 生物班():
+   st.title("生物班")
+
+def 化学班():
+   st.title("化学班")
+
+
+
+#初期表示
+
 if 'pezibangou' not in st.session_state:
    st.session_state.pezibangou = 0
+   初期ページ()
 
 
 
@@ -20,6 +39,17 @@ def 生物班():
 
 def 化学班():
    st.title("化学班")
+
+def ページ移動():
+
+   if st.session_state.pezibangou == 0:
+      初期ページ()
+   elif st.session_state.pezibangou == 1:
+      工学班()
+   elif st.session_state.pezibangou == 2:
+      生物班()
+   else:
+      化学班()
 
 
 
@@ -38,19 +68,6 @@ if st.sidebar.button("科学部生物班"):
 
 if st.sidebar.button("科学部化学班"):
    st.session_state.pezibangou = 3
-
-
-
-#ページ移動
-
-if st.session_state.pezibangou == 0:
-   初期ページ()
-elif st.session_state.pezibangou == 1:
-   工学班()
-elif st.session_state.pezibangou == 2:
-   生物班()
-else:
-   化学班()
 
 
 
