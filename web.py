8b53102032b494827fbb0b62b37ee97d7f2f12b1
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-12 15:47:36 +0900

    あ

--- web.py
+++ web.py
@@ -1,59 +1,51 @@
 import streamlit as st
 
-
 #初期設定
 
-if 'pezibangou' not in st.session_state:
-
-   st.session_state.pezibangou = 0
+st.session_state.工学班 == False
+st.session_state.生物班 == False
+st.session_state.化学班 == False
 
 def 工学班():
    st.title("工学班")
 def 生物班():
-   st.title("生物班")
+   st.title("工学班")
 def 化学班():
    st.title("化学班")
 
-def ボタン():
-   
-   if st.button("科学部工学班掲示板 "):
-      st.session_state.pezibangou = 1
+def 工学班ページ():
+   if st.session_state.工学班 == True:
       工学班()
-   if st.button("科学部生物班掲示板 "):
-      st.session_state.pezibangou = 2
+def 生物班ページ():
+   if st.session_state.工学班 == True:
       生物班()
-   if st.button("科学部化学班掲示板 "):
-      st.session_state.pezibangou = 3
+def 化学班ページ():
+   if st.session_state.工学班 == True:
       化学班()
 
+def 班ページ():
+   if st.button("工学班 "):
+      if st.session_state.工学班 == False:
+         st.session_state.工学班 = True
+      else:
+         st.session_state.工学班 = False
+      工学班ページ()
+   if st.button("生物班 "):
+      if st.session_state.生物班 == False:
+         st.session_state.生物班 = True
+      else:
+         st.session_state.生物班 = False
+      生物班ページ()
+   if st.button("化学班 "):
+      if st.session_state.化学班 == False:
+         st.session_state.化学班 = True
+      else:
+         st.session_state.化学班 = False
+      化学班ページ()
 
-"""def サイドバーボタン():
 
-   st.sidebar.title("目次")
-   
-   if st.sidebar.button("中等部科学部掲示板"):
-      st.session_state.pezibangou = 0
-      ページ移動()
-   if st.sidebar.button("科学部工学班掲示板"):
-      st.session_state.pezibangou = 1
-      ページ移動()
-   if st.sidebar.button("科学部生物班掲示板"):
-      st.session_state.pezibangou = 2
-      ページ移動()
-   if st.sidebar.button("科学部化学班掲示板"):
-      st.session_state.pezibangou = 3
-      ページ移動()"""
-
-def ページ移動():
-
-   if st.session_state.pezibangou == 1:
-      工学班()
-   elif st.session_state.pezibangou == 2:
-      生物班()
-   else:
-      化学班()
-
+#ページ表示
 st.title("中等部科学部掲示板")
-
-ボタン()
+#班ごとの掲示板表示
+班ページ()
 
