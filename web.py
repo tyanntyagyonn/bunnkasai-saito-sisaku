324be6a1a64fb1d24e3e2d532593cd5824aad9e6
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-12 15:09:16 +0900

    あ

--- web.py
+++ web.py
@@ -16,66 +16,57 @@ def 生物班():
 def 化学班():
    st.title("化学班")
 
-def ページ移動():
-
-   if st.session_state.pezibangou == 0:
-      初期ページ()
-   elif st.session_state.pezibangou == 1:
-      工学班()
-   elif st.session_state.pezibangou == 2:
-      生物班()
-   else:
-      化学班()
-
 def ボタン():
    
    if st.button("中等部科学部掲示板 "):
-
       st.session_state.pezibangou = 0
-
+      ページ移動()
    if st.button("科学部工学班掲示板 "):
-   
       st.session_state.pezibangou = 1
-
+      ページ移動()
    if st.button("科学部生物班掲示板 "):
-
       st.session_state.pezibangou = 2
-
+      ページ移動()
    if st.button("科学部化学班掲示板 "):
-
       st.session_state.pezibangou = 3
+      ページ移動()
 
 
 def サイドバーボタン():
 
    st.sidebar.title("目次")
-
+   
    if st.sidebar.button("中等部科学部掲示板"):
-
       st.session_state.pezibangou = 0
-
+      ページ移動()
    if st.sidebar.button("科学部工学班掲示板"):
-
       st.session_state.pezibangou = 1
-
+      ページ移動()
    if st.sidebar.button("科学部生物班掲示板"):
-
       st.session_state.pezibangou = 2
-
+      ページ移動()
    if st.sidebar.button("科学部化学班掲示板"):
-
       st.session_state.pezibangou = 3
+      ページ移動()
+
+def ページ移動():
+
+   if st.session_state.pezibangou == 0:
+      初期ページ()
+      ボタン()
+   elif st.session_state.pezibangou == 1:
+      工学班()
+      ボタン()
+   elif st.session_state.pezibangou == 2:
+      生物班()
+      ボタン()
+   else:
+      化学班()
+      ボタン()
+
 
 if 'pezibangou' not in st.session_state:
 
    st.session_state.pezibangou = 0
    サイドバーボタン()
    ページ移動()
-   ボタン()
-
-
-
-#ページ表示
-サイドバーボタン()
-ページ移動()
-ボタン()
