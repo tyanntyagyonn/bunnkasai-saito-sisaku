054fd1242868a6a24b9a0039391ea50a86970876
{tyanntyagyonn} <{sousatannsiyone104@gmail.com}>
2022-06-12 15:03:01 +0900

    あ

--- web.py
+++ web.py
@@ -29,19 +29,19 @@ def ページ移動():
 
 def ボタン():
    
-   if st.button("中等部科学部 "):
+   if st.button("中等部科学部掲示板 "):
 
       st.session_state.pezibangou = 0
 
-   if st.button("科学部工学班 "):
+   if st.button("科学部工学班掲示板 "):
    
       st.session_state.pezibangou = 1
 
-   if st.button("科学部生物班 "):
+   if st.button("科学部生物班掲示板 "):
 
       st.session_state.pezibangou = 2
 
-   if st.button("科学部化学班 "):
+   if st.button("科学部化学班掲示板 "):
 
       st.session_state.pezibangou = 3
 
@@ -50,19 +50,19 @@ def サイドバーボタン():
 
    st.sidebar.title("目次")
 
-   if st.sidebar.button("中等部科学部"):
+   if st.sidebar.button("中等部科学部掲示板"):
 
       st.session_state.pezibangou = 0
 
-   if st.sidebar.button("科学部工学班"):
+   if st.sidebar.button("科学部工学班掲示板"):
 
       st.session_state.pezibangou = 1
 
-   if st.sidebar.button("科学部生物班"):
+   if st.sidebar.button("科学部生物班掲示板"):
 
       st.session_state.pezibangou = 2
 
-   if st.sidebar.button("科学部化学班"):
+   if st.sidebar.button("科学部化学班掲示板"):
 
       st.session_state.pezibangou = 3
 
